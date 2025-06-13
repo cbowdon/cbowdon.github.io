8b6c4bad601334ec2f8c98595fcc62db71927113
Chris Bowdon <cbowdon@example.com>
2025-06-13 17:26:57 +0100

    Why can't we just count mentions?

--- posts/food/articles.R
+++ posts/food/articles.R
@@ -10,7 +10,6 @@ DOMAINS <- c(
   "telegraph.co.uk",
   "standard.co.uk",
   "metro.co.uk",
-  "thetimes.co.uk",
   "ft.com"
 )
 
@@ -21,7 +20,9 @@ load_upf_articles <- function() {
   ) |>
     unnest("_source") |>
     unnest("highlight") |>
+    filter(domain %in% DOMAINS) |>
     mutate(
+      domain = factor(domain, levels = DOMAINS, ordered = TRUE),
       harvestTime = as.Date(harvestTime),
       period = as.Date(strftime(harvestTime, "%Y-%m-01")),
       is_focus = str_detect(str_to_lower(title), "ultra")
@@ -35,12 +36,16 @@ load_upf_articles <- function() {
 load_domain_counts <- function() {
   jsonlite::read_json("data/aggs.json", simplifyVector = T) |>
     rename(domain = key, total = doc_count) |>
+    filter(domain %in% DOMAINS) |>
+    mutate(domain = factor(domain, levels = DOMAINS, ordered = TRUE)) |>
     unnest("by_date") |>
     unnest("buckets") |>
     rename(period = key_as_string, n_articles = doc_count) |>
+    mutate(period = as.Date(period)) |>
+    filter(period >= "2024-01-01" & period < "2025-06-01") |>
     select(-key)
 }
 
 # ggplot(domain_counts) + aes(x = period, y = n_articles, group = domain, colour = domain) + geom_line()
 
-# ggplot(upf_articles) + aes(x = period) + geom_histogram(stat = "count")
+# ggplot(upf_articles) + aes(x = period, group=domain, fill=domain) + geom_histogram(stat = "count")
